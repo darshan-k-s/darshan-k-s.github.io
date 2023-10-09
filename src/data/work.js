2576ffc14f49a3fb2cb2d6a3f5dd8fec9c9d164c
darshan-k-s <darshanks.work@gmail.com>
2023-10-09 22:42:29 +0530

    Added RBCCPS

--- src/data/work.js
+++ src/data/work.js
@@ -1,6 +1,15 @@
 const work = [
     {
         id: 1,
+        company: "Robert Bosch Centre for Cyber-Physical Systems",
+        title: "Development Intern",
+        duration: "Dec 2022 - Jan 2023",
+        description: "Worked on the development of the software stack(ROS) and hardware interface for a 3DOF articulated arm for the STOCH - a quadruped targeted for pesticide spraying application.<br/>Supervisor: Dr. Shishir Kolathaya",
+        imgsrc: "rbccps.png",
+        site: "https://cps.iisc.ac.in/"
+    },
+    {
+        id: 2,
         company: "Project MANAS",
         title: "Sensing and Automation division member",
         duration: "May 2021 - Present",
@@ -9,7 +18,7 @@ const work = [
         site: "https://www.projectmanas.in/"
     },
     {
-        id: 2,
+        id: 3,
         company: "Formi",
         title: "Frontend Developer",
         duration: "Jan 2022 - Apr 2022",
